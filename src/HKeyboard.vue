77e0b7038c462b080e054f0e8225d01809c5b034
quietmint <quietmint@users.noreply.github.com>
2021-07-07 08:31:47 -0400

    44407: Fix dark mode keyboard

--- src/HKeyboard.vue
+++ src/HKeyboard.vue
@@ -4,7 +4,7 @@
       <div class="flex flex-col items-center justify-center bg-white text-gray-800 dark:bg-gray-900 dark:text-gray-300 rounded-lg shadow p-4" @click.stop>
         <div class="text-20 mb-4" v-text="i18n('keyboard')"></div>
         <div v-for="row in rows" :key="row" class="flex justify-center">
-          <div v-for="letter in row" :key="letter" @click.stop="click(letter)" class="flex-none cursor-pointer text-center m-2 w-14 h-14 leading-14 shadow text-24 font-bold rounded-full bg-gradient-to-b from-gray-100 via-white to-gray-200 dark:from-gray-700 dark:to-gray-800 hover:from-blue-600 hover:to-blue-700 hover:text-white">{{ letter }}</div>
+          <div v-for="letter in row" :key="letter" @click.stop="click(letter)" class="flex-none cursor-pointer text-center m-2 w-14 h-14 leading-14 shadow text-24 font-bold rounded-full bg-gradient-to-b from-gray-100 via-gray-100 to-gray-200 dark:from-gray-700 dark:via-gray-700 dark:to-gray-800 hover:from-blue-600 hover:to-blue-700 hover:text-white">{{ letter }}</div>
         </div>
       </div>
     </div>
